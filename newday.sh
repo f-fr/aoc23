114790c566cdfb99b80f186907a822cb17e17fe1
Frank Fischer <frank-fischer@shadow-soft.de>
2023-11-30 00:33:21 +0100

    newday.sh: ensure "input" directory exists.

--- newday.sh
+++ newday.sh
@@ -4,7 +4,7 @@ pth=$(dirname -- "$0")
 day=$(printf "%02d" "$1")
 pushd $pth >& /dev/null
 mkdir "src/$day"
-mkdir "input/$day"
+mkdir -p "input/$day"
 cp -i "src/template_main.zig" "src/$day/main.zig"
 sed -i -e "s/DAY/$day/" "src/$day/main.zig"
 hg add "src/$day/main.zig"
