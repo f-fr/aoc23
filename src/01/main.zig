7f71d7920713af53884987e550501547067f3d40
Frank Fischer <frank-fischer@shadow-soft.de>
2023-12-01 10:52:38 +0100

    01: refactor

--- src/01/main.zig
+++ src/01/main.zig
@@ -2,36 +2,36 @@
 const std = @import("std");
 const aoc = @import("aoc");
 
-pub fn run(lines: *aoc.Lines) ![2]u64 {
+fn findNums(comptime dir: enum { fwd, bwd }, line: []const u8) [2]u64 {
     const words = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    const start = if (dir == .fwd) 0 else line.len - 1;
+    var d1: u64 = 100;
+    var d2: u64 = 100;
+    for (0..line.len) |i| {
+        const pos = if (dir == .fwd) start + i else start - i;
+        if ('0' <= line[pos] and line[pos] <= '9') {
+            d1 = line[pos] - '0';
+            if (d2 == 100) d2 = d1;
+            break;
+        } else if (d2 == 100) {
+            for (words, 1..) |word, x| if (std.mem.startsWith(u8, line[pos..], word)) {
+                d2 = x;
+                break;
+            };
+        }
+    }
+    return .{ d1, d2 };
+}
+
+pub fn run(lines: *aoc.Lines) ![2]u64 {
     var sum1: u64 = 0;
     var sum2: u64 = 0;
 
     while (try lines.next()) |line| {
-        var n1: u64 = 0;
-        var n2: u64 = 0;
-        inline for (.{ .{ 0, 1 }, .{ line.len - 1, line.len - 1 } }) |s| {
-            var d1: u64 = 100;
-            var d2: u64 = 100;
-            for (0..line.len) |i| {
-                const pos = (s[0] + s[1] * i) % line.len;
-                if ('0' <= line[pos] and line[pos] <= '9') {
-                    d1 = line[pos] - '0';
-                    if (d2 == 100) d2 = d1;
-                    break;
-                } else if (d2 == 100) {
-                    for (words, 1..) |word, x| if (std.mem.startsWith(u8, line[pos..], word)) {
-                        d2 = x;
-                        break;
-                    };
-                }
-            }
-            n1 = 10 * n1 + d1;
-            n2 = 10 * n2 + d2;
-        }
-
-        sum1 += n1;
-        sum2 += n2;
+        const n = findNums(.fwd, line);
+        const m = findNums(.bwd, line);
+        sum1 += n[0] * 10 + m[0];
+        sum2 += n[1] * 10 + m[1];
     }
 
     return .{ sum1, sum2 };
