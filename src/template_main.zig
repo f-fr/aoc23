3717acf882c94fbaebbfa809bbb74b688f62d103
Frank Fischer <frank-fischer@shadow-soft.de>
2023-12-01 09:08:05 +0100

    template_main.zig: add test case for input0.txt example file

--- src/template_main.zig
+++ src/template_main.zig
@@ -14,3 +14,15 @@ pub fn run(lines: *aoc.Lines) ![2]u64 {
 pub fn main() !void {
     return aoc.run("DAY", run);
 }
+
+test "Day DAY" {
+    const PART1: u64 = 42;
+    const PART2: u64 = 42;
+
+    var lines = try aoc.Lines.init("input/DAY/input0.txt");
+    defer lines.deinit();
+    const scores = try run(&lines);
+
+    try std.testing.expectEqual(PART1, scores[0]);
+    try std.testing.expectEqual(PART2, scores[1]);
+}
